c93fc28a353a49fd4b23cf6eb1c511a1f3f0c18f
sobzelustrelle <slustrelle75@gmail.com>
2026-04-25 12:50:35 +0200

    modification du fichier cors pour autoriser url de production sobze.cdwfs.net

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -1,14 +1,36 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'filiere', 'sanctum/csrf-cookie'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // tu peux remplacer par http://localhost:4200
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
-];
 
-// ISO9126 : Norme de definition d'un logiciel
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    // On autorise localhost pour le dev et ton domaine pour la prod
+    'allowed_origins' => [
+        'http://localhost:4200', 
+        'https://sobze.cdwfs.net'
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
